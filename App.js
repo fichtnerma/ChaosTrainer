785ab7371d9e5801389c401d514e7b3557dbc1a4
Markus Fichtner <fichtnerma76137@th-nuernberg.de>
2020-11-04 10:07:12 +0100

    feature/Navigation

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 //Librarys
 import * as React from 'react';
-import {View, Text, Button} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
--- app/screens/InfoScreen.js
+++ app/screens/InfoScreen.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Text, View, Button} from 'react-native';
 
 export default function InfoScreen({navigation}) {
