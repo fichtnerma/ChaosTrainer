4e14f52b32bbd58a248b0fb1f6299ab384766974
Markus Fichtner <fichtnerma76137@th-nuernberg.de>
2021-01-14 18:19:23 +0100

    feature/InfoScreen

--- app/screens/InfoContainerScreen.js
+++ app/screens/InfoContainerScreen.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import InfoChildScreen from './InfoChildScreen';
 import {Text, StyleSheet, View, Button} from 'react-native';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 
 const Stack = createStackNavigator();
 
@@ -17,11 +18,48 @@ export default function InfoContainerScreen() {
 function InfoScreen({navigation}) {
     return (
         <View style={styles.container}>
-            <Text>Info Screen</Text>
-            <Button
-                title="InfoChild"
-                onPress={() => navigation.navigate('InfoChild')}
-            />
+            <View style={styles.layout}>
+                <TouchableOpacity
+                    style={styles.box}
+                    onPress={() => navigation.navigate('InfoChild')}
+                >
+                    <Text>Erste Hilfe</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.box}
+                    onPress={() => navigation.navigate('InfoChild')}
+                >
+                    <Text>Erste Hilfe</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.layout}>
+                <TouchableOpacity
+                    style={styles.box}
+                    onPress={() => navigation.navigate('InfoChild')}
+                >
+                    <Text>Erste Hilfe</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.box}
+                    onPress={() => navigation.navigate('InfoChild')}
+                >
+                    <Text>Erste Hilfe</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.layout}>
+                <TouchableOpacity
+                    style={styles.box}
+                    onPress={() => navigation.navigate('InfoChild')}
+                >
+                    <Text>Erste Hilfe</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.box}
+                    onPress={() => navigation.navigate('InfoChild')}
+                >
+                    <Text>Erste Hilfe</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     );
 }
@@ -32,17 +70,40 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
+        flexDirection: 'column',
     },
     background: {
         flex: 1,
         width: '100%',
         height: '100%',
     },
+
+    box: {
+        width: 160,
+        height: 160,
+        backgroundColor: '#fff',
+        borderRadius: 30,
+        margin: 10,
+        shadowOffset: {width: 0, height: 0},
+        shadowColor: 'black',
+        shadowOpacity: 0.5,
+        justifyContent: 'center',
+        alignContent: 'center',
+        //shadowColor: '#000',
+        //shadowOpacity: 0.2,
+        shadowRadius: 5,
+        elevation: 10,
+    },
+    layout: {
+        height: '28%',
+        flexDirection: 'row',
+        margin: 10,
+    },
 });
 
 const headerStyle = {
     headerStyle: {
-        backgroundColor: '#6895a2',
+        backgroundColor: '#f79A42',
     },
     headerTintColor: '#fff',
     headerTitleStyle: {
