246c788df8957a09874d94c82392d5adbe049cb0
Markus Fichtner <fichtnerma76137@th-nuernberg.de>
2021-03-16 18:15:46 +0100

    tested json import

--- app/screens/InfoUnterseiten/AsthmaScreen.js
+++ app/screens/InfoUnterseiten/AsthmaScreen.js
@@ -2,31 +2,50 @@ import React from 'react';
 import {Text, StyleSheet, View} from 'react-native';
 import {ScrollView} from 'react-native-gesture-handler';
 
+const data = require('../../assets/content/Infoseiten/Asthma.json');
+
 export default function AsthmaScreen() {
-return(
-<View style={styles.container}> 
-    <Text style={kacheln.titel} >ERSTE HILFE</Text>
-    <Text style={kacheln.h1} >Akute Erkrankungen</Text>
-    <View style={kacheln.infoBox} > 
-        <ScrollView style={{marginBottom:10}}>
-            <Text style={kacheln.h2} >ASTHMA BRONCHIALE</Text>
-            <Text style={kacheln.infoText} >Ursache sind allergische Reaktionen, psychische Einflüsse und 
-            wiederkehrende Infektionen der Luftwege. Dabei entsteht die Atemnot durch eine Verkrampfung oder
-             Verschleimung der tieferliegenden feinen Bronchialäste in der Lunge.</Text>
-             <Text style={kacheln.h2} >ERKENNEN</Text>
-             <Text style={kacheln.infoText} >• Mühe beim Lufteinatmen, das Ausatmen fällt ihm schwer </Text>
-             <Text style={kacheln.infoText} >• Betroffene ringt keuchend nach Luft - infolge des Sauerstoffmangels blau im Gesicht (besonders an den Lippen erkennbar)  </Text>
-             <Text style={kacheln.infoText} >• Betroffener hat Angst und ist sehr unruhig  </Text>
-             <Text style={kacheln.h2} >MAßNAHMEN</Text>
-             <Text style={kacheln.infoText} >• Betroffenen beruhigen und ihn auffordern, bei aufrechtem Oberkörper (stehend oder sitzend) ruhig zu atmen   </Text>
-             <Text style={kacheln.infoText} >• Einnahme von Medikamenten (Inhalation) unterstützen  </Text>
-             <Text style={kacheln.infoText} >• Für Arztbehandlung sorgen  </Text>
-             <Text style={kacheln.infoText} >• Evtl. Notruf   </Text>
-             <Text style={kacheln.infoText} >Weitere Informationen finden Sie unter: https://www.drk.de/hilfe-in-deutschland/erste-hilfe/ </Text>
-        </ScrollView>
-     </View>    
-</View>
-)
+    return (
+        <View style={styles.container}>
+            <Text style={kacheln.titel}>ERSTE HILFE</Text>
+            <Text style={kacheln.h1}>Akute Erkrankungen</Text>
+            <View style={kacheln.infoBox}>
+                <ScrollView style={{marginBottom: 10}}>
+                    <Text style={kacheln.h2}>{data.name}</Text>
+                    <Text style={kacheln.infoText}>{data.cause}</Text>
+                    <Text style={kacheln.h2}>ERKENNEN</Text>
+                    <Text style={kacheln.infoText}>
+                        • Mühe beim Lufteinatmen, das Ausatmen fällt ihm schwer{' '}
+                    </Text>
+                    <Text style={kacheln.infoText}>
+                        • Betroffene ringt keuchend nach Luft - infolge des
+                        Sauerstoffmangels blau im Gesicht (besonders an den
+                        Lippen erkennbar){' '}
+                    </Text>
+                    <Text style={kacheln.infoText}>
+                        • Betroffener hat Angst und ist sehr unruhig{' '}
+                    </Text>
+                    <Text style={kacheln.h2}>MAßNAHMEN</Text>
+                    <Text style={kacheln.infoText}>
+                        • Betroffenen beruhigen und ihn auffordern, bei
+                        aufrechtem Oberkörper (stehend oder sitzend) ruhig zu
+                        atmen{' '}
+                    </Text>
+                    <Text style={kacheln.infoText}>
+                        • Einnahme von Medikamenten (Inhalation) unterstützen{' '}
+                    </Text>
+                    <Text style={kacheln.infoText}>
+                        • Für Arztbehandlung sorgen{' '}
+                    </Text>
+                    <Text style={kacheln.infoText}>• Evtl. Notruf </Text>
+                    <Text style={kacheln.infoText}>
+                        Weitere Informationen finden Sie unter:
+                        https://www.drk.de/hilfe-in-deutschland/erste-hilfe/{' '}
+                    </Text>
+                </ScrollView>
+            </View>
+        </View>
+    );
 }
 const styles = StyleSheet.create({
     container: {
@@ -66,7 +85,6 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         margin: 10,
     },
-
 });
 
 const kacheln = StyleSheet.create({
