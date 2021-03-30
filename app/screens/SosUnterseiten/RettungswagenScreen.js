c23eb22ef2c28067da41ed5bb48738ebc1cd8a5b
Katharinakolm <kolmka76005@th-nuernberg.de>
2021-03-30 19:02:04 +0200

    map test

--- app/screens/SosUnterseiten/RettungswagenScreen.js
+++ app/screens/SosUnterseiten/RettungswagenScreen.js
@@ -1,137 +1,146 @@
 //Librarys
 import React from "react";
-import {Text, StyleSheet, View, Image, Dimensions} from "react-native";
-import {TouchableOpacity} from "react-native-gesture-handler";
+import { Text, StyleSheet, View, Image, Dimensions } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+
+import Colors from "../../constants/colors";
+import Map from "./map";
 
 //Styles
+import mainStyle from "../../components/Styles/mainStyleSheet";
 
 const windowWidth = Dimensions.get("window").width;
 const windowHeight = Dimensions.get("window").height;
 
-export default function RettungswagenScreen({navigation}) {
-    return (
-        <View style={styles.container}>            
-            <View style={kacheln.rufnummerBox}>
-                <Text style={kacheln.h1}>Rettungsdienst</Text>
-                <Text style={kacheln.nummerText}>112</Text>
-                <TouchableOpacity style={kacheln.telefonButton}>
-                    <Image source={require("../../assets/SosHomeScreen/sos_telefon.png")} style={kacheln.telefon} />
-                </TouchableOpacity>
+export default function RettungswagenScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <View style={kacheln.rufnummerBox}>
+        <Text style={kacheln.h1}>Rettungsdienst</Text>
+        <Text style={kacheln.nummerText}>112</Text>
+        <TouchableOpacity style={kacheln.telefonButton}>
+          <Image
+            source={require("../../assets/SosHomeScreen/sos_telefon.png")}
+            style={kacheln.telefon}
+          />
+        </TouchableOpacity>
+      </View>
+      <View /*style={styles.kacheln}*/>
+        <View>
+          <View style={[mainStyle.box, styles.boxSize]}>
+            <View>
+              <Text style={styles.fragezeichen}>?</Text>
             </View>
-            <View style={styles.kacheln}>
-            <View style={styles.layout}>
-                <TouchableOpacity style={styles.box} onPress={() => navigation.navigate("Sos")}>
-                    <Image source={require("../../assets/SosHomeScreen/Rettungswagen.png")} style={kacheln.icons} />
-                    <Text style={kacheln.titel}>Rettungswagen</Text>
-                </TouchableOpacity>
-
-                <TouchableOpacity style={styles.box} onPress={() => navigation.navigate("Sos")}>
-                    <Image source={require("../../assets/SosHomeScreen/Polizei.png")} style={kacheln.icons} />
-                    <Text style={kacheln.titel}>Polizei</Text>
-                </TouchableOpacity>
+            <View>
+              <Text style={mainStyle.h1}>
+                <Text style={{ color: Colors.orange }}>Wo </Text>ist das
+                Ereignis?
+              </Text>
+              <Text style={mainStyle.h1}>
+                <Text style={{ color: Colors.orange }}>Wer </Text>ruft an?
+              </Text>
+              <Text style={mainStyle.h1}>
+                <Text style={{ color: Colors.orange }}>Was </Text>ist geschehen?
+              </Text>
+              <Text style={mainStyle.h1}>
+                <Text style={{ color: Colors.orange }}>Wie </Text>viele
+                Betroffene?
+              </Text>
+              <Text style={mainStyle.h1}>
+                <Text style={{ color: Colors.orange }}>Warten </Text>auf
+                Rückfragen!
+              </Text>
             </View>
-            <View style={styles.layout}>
-                <TouchableOpacity style={styles.box} onPress={() => navigation.navigate("Sos")}>
-                    <Image source={require("../../assets/SosHomeScreen/Gift.png")} style={kacheln.icons} />
-                    <Text style={kacheln.titel}>Gift</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.box} onPress={() => navigation.navigate("Sos")}>
-                    <Image source={require("../../assets/SosHomeScreen/feuerwehr.png")} style={kacheln.icons} />
-                    <Text style={kacheln.titel}>Feuerwehr</Text>
-                </TouchableOpacity>
-            </View>
-            </View>
-            </View>
-    );
+          </View>
+        </View>
+        <Map />
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "flex-end",
-        flexDirection: "column",
-        borderTopLeftRadius: 30,
-        borderTopRightRadius: 30,
-        marginTop: windowHeight * 0.27,
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    flexDirection: "column",
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    marginTop: windowHeight * 0.27,
+  },
 
-    kacheln: {
-        //Die Kacheln müssen weiter nach oben bzw. in der weißen Fläche mittig angezeigt werden
-        //justifyContent: "center",
-        //margin: windowHeight * -0.02,
-    },
+  /*kacheln: {
+    flex: 4,
+  },*/
 
-    box: {
-        width: windowWidth * 0.4, //160
-        height: windowWidth * 0.4,
-        backgroundColor: "#fff",
-        borderRadius: 15,
-        margin: 10,
-        shadowOffset: {width: 0, height: 0},
-        shadowColor: "black",
-        shadowOpacity: 0.5,
-        shadowRadius: 5,
-        elevation: 10,
-        alignItems: "center",
-    },
-    layout: {
-        flexDirection: "row",
-        margin: 10,
-    },
+  boxSize: {
+    width: windowWidth * 0.9, //160
+    height: windowWidth * 0.3,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  fragezeichen: {
+    fontSize: 120,
+    fontWeight: "bold",
+    color: Colors.orange,
+    marginRight: 25,
+  },
 });
 
 const kacheln = StyleSheet.create({
-    icons: {
-        width: windowWidth * 0.4,
-        height: windowWidth * 0.4,
-        flex: 5,
-    },
-    titel: {
-        justifyContent: "center",
-        color: "#14647f",
-        flex: 1,
-        fontSize: 16,
-        marginBottom: 15,
-    },
-    h1: {
-        //height: windowHeight,
-        justifyContent: "center",
-        color: "#fff",
-        fontSize: 20,
-        marginBottom: 5,
-    },
-    nummerText: {
-        color: "#fff",
-        fontSize: 30,
-        marginBottom: 0,
-        fontWeight: "bold",
-        alignSelf: "center",
-    },
-    rufnummerBox: {
-        transform: [{translateY: -30}],
-        height: windowHeight * 0.25,
-    },
-    telefonButton: {
-        width: windowWidth * 0.2,
-        height: windowWidth * 0.2,
-        marginTop: 5,
-        marginBottom: 20,
-        alignSelf: "center",
-        backgroundColor: "#fff",
-        borderRadius: 15,
-        shadowOffset: {width: 0, height: 0},
-        shadowColor: "black",
-        shadowOpacity: 0.5,
-        shadowRadius: 5,
-        elevation: 10,
-        justifyContent: "center",
-    },
-    telefon: {
-        width: windowWidth * 0.3, //100
-        height: windowWidth * 0.3,
-        marginTop: 0,
-        alignSelf: "center",
-    },
+  icons: {
+    width: windowWidth * 0.4,
+    height: windowWidth * 0.4,
+    flex: 5,
+  },
+  titel: {
+    justifyContent: "center",
+    color: "#14647f",
+    flex: 1,
+    fontSize: 16,
+    marginBottom: 15,
+  },
+  h1: {
+    //height: windowHeight,
+    justifyContent: "center",
+    color: "#fff",
+    fontSize: 20,
+    marginBottom: 5,
+  },
+  nummerText: {
+    color: "#fff",
+    fontSize: 30,
+    marginBottom: 0,
+    fontWeight: "bold",
+    alignSelf: "center",
+  },
+  rufnummerBox: {
+    transform: [{ translateY: -30 }],
+    height: windowHeight * 0.2,
+  },
+  telefonButton: {
+    width: windowWidth * 0.2,
+    height: windowWidth * 0.2,
+    marginTop: 5,
+    marginBottom: 20,
+    alignSelf: "center",
+    backgroundColor: "#fff",
+    borderRadius: 15,
+    shadowOffset: { width: 0, height: 0 },
+    shadowColor: "black",
+    shadowOpacity: 0.5,
+    shadowRadius: 5,
+    elevation: 10,
+    justifyContent: "center",
+  },
+  telefon: {
+    width: windowWidth * 0.3, //100
+    height: windowWidth * 0.3,
+    marginTop: 0,
+    alignSelf: "center",
+  },
 });
---
+++ app/screens/SosUnterseiten/map.js
@@ -0,0 +1,111 @@
+import React from "react";
+import { View, Text, PermissionsAndroid, Alert, Platform } from "react-native";
+import Geolocation from "react-native-geolocation-service";
+import MapView, { PROVIDER_GOOGLE, Marker, Polyline } from "react-native-maps";
+
+export default class Map extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      latitude: 0,
+      longitude: 0,
+      coordinates: [],
+    };
+  }
+
+  async componentDidMount() {
+    Geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          coordinates: this.state.coordinates.concat({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          }),
+        });
+      },
+      (error) => {
+        Alert.alert(error.message.toString());
+      },
+      {
+        showLocationDialog: true,
+        enableHighAccuracy: true,
+        timeout: 20000,
+        maximumAge: 0,
+      }
+    );
+
+    Geolocation.watchPosition(
+      (position) => {
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          coordinates: this.state.coordinates.concat({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          }),
+        });
+      },
+      (error) => {
+        console.log(error);
+      },
+      {
+        showLocationDialog: true,
+        enableHighAccuracy: true,
+        timeout: 20000,
+        maximumAge: 0,
+        distanceFilter: 0,
+      }
+    );
+  }
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <MapView
+          provider={PROVIDER_GOOGLE}
+          customMapStyle={styles.map}
+          style={{ flex: 1 }}
+          region={{
+            latitude: this.state.latitude,
+            longitude: this.state.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        >
+          <Marker
+            coordinate={{
+              latitude: this.state.latitude,
+              longitude: this.state.longitude,
+            }}
+          ></Marker>
+          <Polyline
+            coordinates={this.state.coordinates}
+            strokeColor="#bf8221"
+            strokeColors={[
+              "#bf8221",
+              "#ffe066",
+              "#ffe066",
+              "#ffe066",
+              "#ffe066",
+            ]}
+            strokeWidth={3}
+          />
+        </MapView>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  map: {
+    width: Dimensions.get("window").width * 0.9,
+    height: Dimensions.get("window").height * 0.35,
+  },
+});
