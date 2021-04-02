c4ca1869305527896740a3231c09fbf108242a93
Tabea Schaeffer <schaefferta77456@th-nuernberg.de>
2021-04-02 12:40:40 +0200

    location

--- app/screens/SosUnterseiten/map.js
+++ app/screens/SosUnterseiten/map.js
@@ -1,121 +1,119 @@
 import React from "react";
-import {
-  View,
-  StyleSheet,
-  Dimensions,
-  Text,
-  PermissionsAndroid,
-  Alert,
-  Platform,
-} from "react-native";
-import Geolocation from "react-native-geolocation-service";
-import MapView, { PROVIDER_GOOGLE, Marker, Polyline } from "react-native-maps";
+import {StyleSheet, Text, View, Dimensions} from "react-native";
+import * as Location from "expo-location";
+import * as Permissions from "expo-permissions";
+import MapView, {Marker} from "react-native-maps";
+import mainStyle from "../../components/Styles/mainStyleSheet.js";
 
-import { mapsStyle } from "../../components/Styles/mapStyleSheet";
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
 
 export default class Map extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      latitude: 0,
-      longitude: 0,
-      coordinates: [],
+    state = {
+        location: null,
+        geocode: null,
+        errorMessage: "",
     };
-  }
+    componentDidMount() {
+        this.getLocationAsync();
+    }
+    getLocationAsync = async () => {
+        let {status} = await Permissions.askAsync(Permissions.LOCATION);
+        if (status !== "granted") {
+            this.setState({
+                errorMessage: "Permission to access location was denied",
+            });
+        }
 
-  async componentDidMount() {
-    Geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
-          coordinates: this.state.coordinates.concat({
-            latitude: position.coords.latitude,
-            longitude: position.coords.longitude,
-          }),
+        let location = await Location.getCurrentPositionAsync({
+            accuracy: Location.Accuracy.BestForNavigation,
         });
-      },
-      (error) => {
-        Alert.alert(error.message.toString());
-      },
-      {
-        showLocationDialog: true,
-        enableHighAccuracy: true,
-        timeout: 20000,
-        maximumAge: 0,
-      }
-    );
+        const {latitude, longitude} = location.coords;
+        this.getGeocodeAsync({latitude, longitude});
+        this.setState({location: {latitude, longitude}});
+    };
+    getGeocodeAsync = async (location) => {
+        let geocode = await Location.reverseGeocodeAsync(location);
+        this.setState({geocode});
+    };
 
-    Geolocation.watchPosition(
-      (position) => {
-        this.setState({
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
-          coordinates: this.state.coordinates.concat({
-            latitude: position.coords.latitude,
-            longitude: position.coords.longitude,
-          }),
-        });
-      },
-      (error) => {
-        console.log(error);
-      },
-      {
-        showLocationDialog: true,
-        enableHighAccuracy: true,
-        timeout: 20000,
-        maximumAge: 0,
-        distanceFilter: 0,
-      }
-    );
-  }
-  render() {
-    return (
-      <View style={{ flex: 1 }}>
-        <MapView
-          provider={PROVIDER_GOOGLE}
-          customMapStyle={mapsStyle}
-          style={{ flex: 1 }}
-          region={{
-            latitude: this.state.latitude,
-            longitude: this.state.longitude,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}
-        >
-          <Marker
-            coordinate={{
-              latitude: this.state.latitude,
-              longitude: this.state.longitude,
-            }}
-          ></Marker>
-          <Polyline
-            coordinates={this.state.coordinates}
-            strokeColor="#bf8221"
-            strokeColors={[
-              "#bf8221",
-              "#ffe066",
-              "#ffe066",
-              "#ffe066",
-              "#ffe066",
-            ]}
-            strokeWidth={3}
-          />
-        </MapView>
-      </View>
-    );
-  }
+    render() {
+        const {location, geocode, errorMessage} = this.state;
+        return (
+            <View style={styles.container}>
+                <View style={[mainStyle.box, styles.boxSize]}>
+                    <View>
+                        <Text style={styles.fragezeichen}>?</Text>
+                    </View>
+                    <View style={{marginTop: 20}}>
+                        <Text style={mainStyle.h1}>{geocode ? geocode[0].city : ""}</Text>
+                        <Text style={mainStyle.h1}>
+                            {geocode ? geocode[0].street : ""}, {geocode ? geocode[0].name : ""}
+                        </Text>
+                        <Text style={mainStyle.h1}>
+                            {location ? `${location.latitude}, ${location.longitude}` : ""}
+                        </Text>
+                        <Text style={mainStyle.h1}>{errorMessage}</Text>
+                    </View>
+                </View>
+                <View style={styles.mapContainer}>
+                    <MapView style={styles.map}>
+                        <Marker
+                            coordinate={{
+                                latitude: 0.0922,
+                                longitude: 0.0421,
+                            }}
+                            pinColor="#f79A42"
+                        />
+                    </MapView>
+                </View>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  map: {
-    width: Dimensions.get("window").width * 0.9,
-    height: Dimensions.get("window").height * 0.35,
-  },
+    container: {
+        flex: 1,
+    },
+    mapContainer: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        //borderRadius: 30,
+        //overflow: "hidden",
+    },
+    containerText: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+        zIndex: 20,
+    },
+
+    fragezeichen: {
+        fontSize: 80,
+        fontWeight: "bold",
+        color: "#000",
+        marginRight: 25,
+        marginBottom: 5,
+    },
+    map: {
+        width: windowWidth * 0.9, //160
+        height: windowWidth * 0.63,
+    },
+    heading: {
+        justifyContent: "center",
+        color: "#000",
+        fontSize: 20,
+        marginBottom: 0,
+    },
+    boxSize: {
+        width: windowWidth * 0.85, //160
+        height: windowWidth * 0.2,
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        position: "absolute",
+    },
 });
