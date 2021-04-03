aca8062e31648a9d3fce1ed9453e072705d8efa6
Katharinakolm <kolmka76005@th-nuernberg.de>
2021-04-03 12:11:29 +0200

    map centers on region

--- app/screens/SosUnterseiten/RettungswagenScreen.js
+++ app/screens/SosUnterseiten/RettungswagenScreen.js
@@ -1,7 +1,7 @@
 //Librarys
 import React from "react";
-import {Text, StyleSheet, View, Image, Dimensions} from "react-native";
-import {TouchableOpacity} from "react-native-gesture-handler";
+import { Text, StyleSheet, View, Image, Dimensions } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 import Colors from "../../constants/colors";
 import Map from "./map";
@@ -12,135 +12,134 @@ import mainStyle from "../../components/Styles/mainStyleSheet";
 const windowWidth = Dimensions.get("window").width;
 const windowHeight = Dimensions.get("window").height;
 
-export default function RettungswagenScreen({navigation}) {
-    return (
-        <View style={styles.container}>
-            <View style={kacheln.rufnummerBox}>
-                <Text style={kacheln.h1}>Rettungsdienst</Text>
-                <Text style={kacheln.nummerText}>112</Text>
-                <TouchableOpacity style={kacheln.telefonButton}>
-                    <Image
-                        source={require("../../assets/SosHomeScreen/sos_telefon.png")}
-                        style={kacheln.telefon}
-                    />
-                </TouchableOpacity>
+export default function RettungswagenScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <View style={styles.rufnummerBox}>
+        <Text style={kacheln.h1}>Rettungsdienst</Text>
+        <Text style={kacheln.nummerText}>112</Text>
+        <TouchableOpacity style={kacheln.telefonButton}>
+          <Image
+            source={require("../../assets/SosHomeScreen/sos_telefon.png")}
+            style={kacheln.telefon}
+          />
+        </TouchableOpacity>
+      </View>
+      <View /*style={styles.kacheln}*/>
+        <View>
+          <View style={[mainStyle.box, styles.boxSize]}>
+            <View>
+              <Text style={styles.fragezeichen}>?</Text>
             </View>
-            <View /*style={styles.kacheln}*/>
-                <View>
-                    <View style={[mainStyle.box, styles.boxSize]}>
-                        <View>
-                            <Text style={styles.fragezeichen}>?</Text>
-                        </View>
-                        <View>
-                            <Text style={mainStyle.h1}>
-                                <Text style={{color: Colors.orange}}>Wo </Text>ist das Ereignis?
-                            </Text>
-                            <Text style={mainStyle.h1}>
-                                <Text style={{color: Colors.orange}}>Wer </Text>ruft an?
-                            </Text>
-                            <Text style={mainStyle.h1}>
-                                <Text style={{color: Colors.orange}}>Was </Text>ist geschehen?
-                            </Text>
-                            <Text style={mainStyle.h1}>
-                                <Text style={{color: Colors.orange}}>Wie </Text>viele Betroffene?
-                            </Text>
-                            <Text style={mainStyle.h1}>
-                                <Text style={{color: Colors.orange}}>Warten </Text>auf Rückfragen!
-                            </Text>
-                        </View>
-                    </View>
-                </View>
-                <Map />
+            <View>
+              <Text style={mainStyle.h1}>
+                <Text style={{ color: Colors.orange }}>Wo </Text>ist das
+                Ereignis?
+              </Text>
+              <Text style={mainStyle.h1}>
+                <Text style={{ color: Colors.orange }}>Wer </Text>ruft an?
+              </Text>
+              <Text style={mainStyle.h1}>
+                <Text style={{ color: Colors.orange }}>Was </Text>ist geschehen?
+              </Text>
+              <Text style={mainStyle.h1}>
+                <Text style={{ color: Colors.orange }}>Wie </Text>viele
+                Betroffene?
+              </Text>
+              <Text style={mainStyle.h1}>
+                <Text style={{ color: Colors.orange }}>Warten </Text>auf
+                Rückfragen!
+              </Text>
             </View>
+          </View>
         </View>
-    );
+        <Map />
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "flex-end",
-        flexDirection: "column",
-        borderTopLeftRadius: 30,
-        borderTopRightRadius: 30,
-        marginTop: windowHeight * 0.23,
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    flexDirection: "column",
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    marginTop: windowHeight * 0.24,
+  },
 
-    /*kacheln: {
-    flex: 4,
-  },*/
+  boxSize: {
+    width: windowWidth * 0.85, //160
+    height: windowWidth * 0.3,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 15,
+  },
 
-    boxSize: {
-        width: windowWidth * 0.85, //160
-        height: windowWidth * 0.3,
-        flexDirection: "row",
-        justifyContent: "center",
-        alignItems: "center",
-        marginBottom: 15,
-    },
-
-    fragezeichen: {
-        fontSize: 120,
-        fontWeight: "bold",
-        color: Colors.orange,
-        marginRight: 10,
-        marginLeft: -20,
-        marginBottom: 10,
-    },
+  fragezeichen: {
+    fontSize: 120,
+    fontWeight: "bold",
+    color: Colors.orange,
+    marginRight: 10,
+    marginLeft: -20,
+    marginBottom: 10,
+  },
 });
 
 const kacheln = StyleSheet.create({
-    icons: {
-        width: windowWidth * 0.4,
-        height: windowWidth * 0.4,
-        flex: 5,
-    },
-    titel: {
-        justifyContent: "center",
-        color: "#14647f",
-        flex: 1,
-        fontSize: 16,
-        marginBottom: 15,
-    },
-    h1: {
-        //height: windowHeight,
-        justifyContent: "center",
-        color: "#fff",
-        fontSize: 20,
-        marginBottom: 0,
-    },
-    nummerText: {
-        color: "#fff",
-        fontSize: 30,
-        marginBottom: 0,
-        fontWeight: "bold",
-        alignSelf: "center",
-    },
-    rufnummerBox: {
-        transform: [{translateY: -30}],
-        height: windowHeight * 0.2,
-    },
-    telefonButton: {
-        width: windowWidth * 0.2,
-        height: windowWidth * 0.2,
-        marginTop: 5,
-        marginBottom: 20,
-        alignSelf: "center",
-        backgroundColor: "#fff",
-        borderRadius: 15,
-        shadowOffset: {width: 0, height: 0},
-        shadowColor: "black",
-        shadowOpacity: 0.5,
-        shadowRadius: 5,
-        elevation: 10,
-        justifyContent: "center",
-    },
-    telefon: {
-        width: windowWidth * 0.3, //100
-        height: windowWidth * 0.3,
-        marginTop: 0,
-        alignSelf: "center",
-    },
+  icons: {
+    width: windowWidth * 0.4,
+    height: windowWidth * 0.4,
+    flex: 5,
+  },
+  titel: {
+    justifyContent: "center",
+    color: "#14647f",
+    flex: 1,
+    fontSize: 16,
+    marginBottom: 15,
+  },
+  h1: {
+    //height: windowHeight,
+    justifyContent: "center",
+    color: "#fff",
+    fontSize: 20,
+    marginBottom: 0,
+  },
+  nummerText: {
+    color: "#fff",
+    fontSize: 30,
+    marginBottom: 0,
+    fontWeight: "bold",
+    alignSelf: "center",
+  },
+  rufnummerBox: {
+    transform: [{ translateY: -30 }],
+    height: windowHeight * 0.2,
+  },
+  telefonButton: {
+    width: windowWidth * 0.2,
+    height: windowWidth * 0.2,
+    marginTop: 5,
+    marginBottom: 20,
+    alignSelf: "center",
+    backgroundColor: "#fff",
+    borderRadius: 15,
+    shadowOffset: { width: 0, height: 0 },
+    shadowColor: "black",
+    shadowOpacity: 0.5,
+    shadowRadius: 5,
+    elevation: 10,
+    justifyContent: "center",
+  },
+  telefon: {
+    width: windowWidth * 0.3, //100
+    height: windowWidth * 0.3,
+    marginTop: 0,
+    alignSelf: "center",
+  },
 });
--- app/screens/SosUnterseiten/map.js
+++ app/screens/SosUnterseiten/map.js
@@ -1,119 +1,140 @@
 import React from "react";
-import {StyleSheet, Text, View, Dimensions} from "react-native";
+import { StyleSheet, Text, View, Dimensions, Image } from "react-native";
 import * as Location from "expo-location";
 import * as Permissions from "expo-permissions";
-import MapView, {Marker} from "react-native-maps";
+import MapView, { Marker } from "react-native-maps";
 import mainStyle from "../../components/Styles/mainStyleSheet.js";
 
 const windowWidth = Dimensions.get("window").width;
 const windowHeight = Dimensions.get("window").height;
 
 export default class Map extends React.Component {
-    state = {
-        location: null,
-        geocode: null,
-        errorMessage: "",
-    };
-    componentDidMount() {
-        this.getLocationAsync();
+  state = {
+    mapRegion: null,
+    location: null,
+    geocode: null,
+    errorMessage: "",
+  };
+  componentDidMount() {
+    this.getLocationAsync();
+  }
+  handleMapRegionChange = (mapRegion) => {
+    this.setState({ mapRegion });
+  };
+  getLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    //asks for user permission
+    if (status !== "granted") {
+      this.setState({
+        errorMessage: "Permission to access location was denied",
+      });
     }
-    getLocationAsync = async () => {
-        let {status} = await Permissions.askAsync(Permissions.LOCATION);
-        if (status !== "granted") {
-            this.setState({
-                errorMessage: "Permission to access location was denied",
-            });
-        }
 
-        let location = await Location.getCurrentPositionAsync({
-            accuracy: Location.Accuracy.BestForNavigation,
-        });
-        const {latitude, longitude} = location.coords;
-        this.getGeocodeAsync({latitude, longitude});
-        this.setState({location: {latitude, longitude}});
-    };
-    getGeocodeAsync = async (location) => {
-        let geocode = await Location.reverseGeocodeAsync(location);
-        this.setState({geocode});
-    };
+    let location = await Location.getCurrentPositionAsync({
+      accuracy: Location.Accuracy.BestForNavigation,
+    });
+    //centers Map on the location
+    this.setState({
+      mapRegion: {
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      },
+    });
+    const { latitude, longitude } = location.coords;
+    this.getGeocodeAsync({ latitude, longitude });
+    this.setState({ location: { latitude, longitude } });
+  };
+  getGeocodeAsync = async (location) => {
+    let geocode = await Location.reverseGeocodeAsync(location);
+    this.setState({ geocode });
+  };
 
-    render() {
-        const {location, geocode, errorMessage} = this.state;
-        return (
-            <View style={styles.container}>
-                <View style={[mainStyle.box, styles.boxSize]}>
-                    <View>
-                        <Text style={styles.fragezeichen}>?</Text>
-                    </View>
-                    <View style={{marginTop: 20}}>
-                        <Text style={mainStyle.h1}>{geocode ? geocode[0].city : ""}</Text>
-                        <Text style={mainStyle.h1}>
-                            {geocode ? geocode[0].street : ""}, {geocode ? geocode[0].name : ""}
-                        </Text>
-                        <Text style={mainStyle.h1}>
-                            {location ? `${location.latitude}, ${location.longitude}` : ""}
-                        </Text>
-                        <Text style={mainStyle.h1}>{errorMessage}</Text>
-                    </View>
-                </View>
-                <View style={styles.mapContainer}>
-                    <MapView style={styles.map}>
-                        <Marker
-                            coordinate={{
-                                latitude: 0.0922,
-                                longitude: 0.0421,
-                            }}
-                            pinColor="#f79A42"
-                        />
-                    </MapView>
-                </View>
-            </View>
-        );
-    }
+  render() {
+    const { location, geocode, errorMessage } = this.state;
+    return (
+      <View style={styles.container}>
+        <View style={[mainStyle.box, styles.boxSize]}>
+          <View>
+            <Image
+              source={require("../../assets/SosHomeScreen/location.png")}
+              style={styles.locationIcon}
+            />
+          </View>
+          <View style={{ marginTop: 20 }}>
+            <Text style={mainStyle.h1}>{geocode ? geocode[0].city : ""}</Text>
+            <Text style={mainStyle.h1}>
+              {geocode ? geocode[0].street : ""}{" "}
+              {geocode ? geocode[0].name : ""}
+            </Text>
+            <Text style={mainStyle.h1}>
+              {location ? `${location.latitude}, ${location.longitude}` : ""}
+            </Text>
+            <Text style={mainStyle.h1}>{errorMessage}</Text>
+          </View>
+        </View>
+        <View style={styles.mapContainer}>
+          <MapView
+            style={styles.map}
+            region={this.state.mapRegion}
+            onRegionChange={this.handleMapRegionChange}
+          >
+            <Marker
+              coordinate={{
+                latitude: 0.0922,
+                longitude: 0.0421,
+              }}
+              pinColor="#f79A42"
+            />
+          </MapView>
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    mapContainer: {
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        //borderRadius: 30,
-        //overflow: "hidden",
-    },
-    containerText: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-        zIndex: 20,
-    },
+  container: {
+    flex: 1,
+    alignItems: "center",
+  },
+  mapContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    //borderRadius: 30,
+    //overflow: "hidden",
+  },
+  containerText: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    zIndex: 20,
+  },
 
-    fragezeichen: {
-        fontSize: 80,
-        fontWeight: "bold",
-        color: "#000",
-        marginRight: 25,
-        marginBottom: 5,
-    },
-    map: {
-        width: windowWidth * 0.9, //160
-        height: windowWidth * 0.63,
-    },
-    heading: {
-        justifyContent: "center",
-        color: "#000",
-        fontSize: 20,
-        marginBottom: 0,
-    },
-    boxSize: {
-        width: windowWidth * 0.85, //160
-        height: windowWidth * 0.2,
-        flexDirection: "row",
-        justifyContent: "center",
-        alignItems: "center",
-        position: "absolute",
-    },
+  locationIcon: {
+    width: windowWidth * 0.15,
+    height: windowWidth * 0.15,
+    marginRight: 20,
+    marginLeft: -20,
+  },
+  map: {
+    width: windowWidth * 0.9, //160
+    height: windowWidth * 0.75,
+  },
+  heading: {
+    justifyContent: "center",
+    color: "#000",
+    fontSize: 20,
+  },
+  boxSize: {
+    width: windowWidth * 0.85, //160
+    height: windowWidth * 0.2,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+  },
 });
