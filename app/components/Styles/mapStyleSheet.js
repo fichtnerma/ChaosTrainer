7650ad8653d760dd4b30ebd47fd1ae27e27588ef
Markus Fichtner <fichtnerma76137@th-nuernberg.de>
2021-04-10 11:32:29 +0200

    Ios hinzugefügt

--- app/components/Styles/mapStyleSheet.js
+++ app/components/Styles/mapStyleSheet.js
@@ -1,50 +1,51 @@
 //Librarys
-import { StyleSheet, Dimensions } from "react-native";
-import Colors from "../../constants/colors.js";
+import {StyleSheet, Dimensions} from 'react-native';
+import Colors from '../../constants/colors.js';
 
-const windowWidth = Dimensions.get("window").width;
-const windowHeight = Dimensions.get("window").height;
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 
 export default StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-  },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+    },
 
-  mapContainer: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    //borderRadius: 30,
-    //overflow: "hidden",
-  },
+    mapContainer: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 20,
+        overflow: 'hidden',
+    },
 
-  containerText: {
-    flex: 1,
-    backgroundColor: Colors.white,
-    alignItems: "center",
-    justifyContent: "center",
-    zIndex: 20,
-  },
+    containerText: {
+        flex: 1,
+        backgroundColor: Colors.white,
+        alignItems: 'center',
+        justifyContent: 'center',
+        zIndex: 20,
+    },
 
-  locationIcon: {
-    width: windowWidth * 0.15,
-    height: windowWidth * 0.15,
-    marginRight: 20,
-    marginLeft: -20,
-  },
+    locationIcon: {
+        width: windowWidth * 0.15,
+        height: windowWidth * 0.15,
+        marginRight: 20,
+        marginLeft: 25,
+    },
 
-  map: {
-    width: windowWidth * 0.9, //160
-    height: windowWidth * 0.75,
-  },
+    map: {
+        width: windowWidth * 0.9, //160
+        height: windowHeight * 0.39,
+    },
 
-  boxSize: {
-    width: windowWidth * 0.85, //160
-    height: windowWidth * 0.2,
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-    position: "absolute",
-  },
+    boxSize: {
+        width: windowWidth * 0.85, //160
+        height: windowHeight * 0.1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'absolute',
+        zIndex: 90,
+    },
 });
--- app/screens/SosUnterseiten/RettungswagenScreen.js
+++ app/screens/SosUnterseiten/RettungswagenScreen.js
@@ -1,57 +1,51 @@
 //Librarys
-import React from "react";
-import { Text, View, Image } from "react-native";
-import { TouchableOpacity } from "react-native-gesture-handler";
-import Colors from "../../constants/colors";
-import Map from "./map";
+import React from 'react';
+import {Text, View, Image} from 'react-native';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import Colors from '../../constants/colors';
+import Map from '../../components/map';
 
 //Styles
-import mainStyle from "../../components/Styles/mainStyleSheet";
-import sosUnterseiteStyle from "../../components/Styles/sosUnterseiteStyleSheet";
+import mainStyle from '../../components/Styles/mainStyleSheet';
+import sosUnterseiteStyle from '../../components/Styles/sosUnterseiteStyleSheet';
 
-export default function RettungswagenScreen({ navigation }) {
-  return (
-    <View style={sosUnterseiteStyle.container}>
-      <View style={sosUnterseiteStyle.rufnummerBox}>
-        <Text style={sosUnterseiteStyle.h1}>Rettungsdienst</Text>
-        <Text style={sosUnterseiteStyle.nummerText}>112</Text>
-        <TouchableOpacity style={sosUnterseiteStyle.telefonButton}>
-          <Image
-            source={require("../../assets/SosHomeScreen/sos_telefon.png")}
-            style={sosUnterseiteStyle.telefon}
-          />
-        </TouchableOpacity>
-      </View>
-      <View>
-        <View>
-          <View style={[mainStyle.box, sosUnterseiteStyle.boxSize]}>
-            <View>
-              <Text style={sosUnterseiteStyle.fragezeichen}>?</Text>
+export default function RettungswagenScreen({navigation}) {
+    return (
+        <View style={sosUnterseiteStyle.container}>
+            <View style={sosUnterseiteStyle.rufnummerBox}>
+                <Text style={sosUnterseiteStyle.h1}>Rettungsdienst</Text>
+                <Text style={sosUnterseiteStyle.nummerText}>112</Text>
+                <TouchableOpacity style={sosUnterseiteStyle.telefonButton}>
+                    <Image source={require('../../assets/SosHomeScreen/sos_telefon.png')} style={sosUnterseiteStyle.telefon} />
+                </TouchableOpacity>
             </View>
             <View>
-              <Text style={mainStyle.h1}>
-                <Text style={{ color: Colors.orange }}>Wo </Text>ist das
-                Ereignis?
-              </Text>
-              <Text style={mainStyle.h1}>
-                <Text style={{ color: Colors.orange }}>Wer </Text>ruft an?
-              </Text>
-              <Text style={mainStyle.h1}>
-                <Text style={{ color: Colors.orange }}>Was </Text>ist geschehen?
-              </Text>
-              <Text style={mainStyle.h1}>
-                <Text style={{ color: Colors.orange }}>Wie </Text>viele
-                Betroffene?
-              </Text>
-              <Text style={mainStyle.h1}>
-                <Text style={{ color: Colors.orange }}>Warten </Text>auf
-                Rückfragen!
-              </Text>
+                <View>
+                    <View style={[mainStyle.box, sosUnterseiteStyle.boxSize]}>
+                        <View>
+                            <Text style={sosUnterseiteStyle.fragezeichen}>?</Text>
+                        </View>
+                        <View>
+                            <Text style={mainStyle.h1}>
+                                <Text style={{color: Colors.orange}}>Wo </Text>ist das Ereignis?
+                            </Text>
+                            <Text style={mainStyle.h1}>
+                                <Text style={{color: Colors.orange}}>Wer </Text>ruft an?
+                            </Text>
+                            <Text style={mainStyle.h1}>
+                                <Text style={{color: Colors.orange}}>Was </Text>ist geschehen?
+                            </Text>
+                            <Text style={mainStyle.h1}>
+                                <Text style={{color: Colors.orange}}>Wie </Text>viele Betroffene?
+                            </Text>
+                            <Text style={mainStyle.h1}>
+                                <Text style={{color: Colors.orange}}>Warten </Text>auf Rückfragen!
+                            </Text>
+                        </View>
+                    </View>
+                </View>
+                <Map />
             </View>
-          </View>
         </View>
-        <Map />
-      </View>
-    </View>
-  );
+    );
 }
