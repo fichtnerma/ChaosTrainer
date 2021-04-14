5f0114792aaadf100c0cccfb44eb351820a8082e
Tabea Schaeffer <schaefferta77456@th-nuernberg.de>
2021-04-14 15:11:04 +0200

    nummer geändert

--- app/screens/SosUnterseiten/RettungswagenScreen.js
+++ app/screens/SosUnterseiten/RettungswagenScreen.js
@@ -1,26 +1,24 @@
 //Librarys
-import React from 'react';
-import {Text, View, Image} from 'react-native';
-import {TouchableOpacity} from 'react-native-gesture-handler';
-import Colors from '../../constants/colors';
-import Map from '../../components/map';
+import React from "react";
+import {Text, View, Image} from "react-native";
+import {TouchableOpacity} from "react-native-gesture-handler";
+import Colors from "../../constants/colors";
+import Map from "../../components/map";
 import call from "react-native-phone-call";
 
 //Styles
-import mainStyle from '../../components/Styles/mainStyleSheet';
-import sosUnterseiteStyle from '../../components/Styles/sosUnterseiteStyleSheet';
-
-
+import mainStyle from "../../components/Styles/mainStyleSheet";
+import sosUnterseiteStyle from "../../components/Styles/sosUnterseiteStyleSheet";
 
 export default function RettungswagenScreen({navigation}) {
-
     const triggerCall = () => {
-    const args = {
-        number: "112",
-        prompt: true,
+        const args = {
+            // Nummer muss noch geändert werden
+            number: "01739965411",
+            prompt: true,
+        };
+        call(args).catch(console.error);
     };
-    call(args).catch(console.error);
-};
 
     return (
         <View style={sosUnterseiteStyle.container}>
@@ -28,7 +26,10 @@ export default function RettungswagenScreen({navigation}) {
                 <Text style={sosUnterseiteStyle.h1}>Rettungsdienst</Text>
                 <Text style={sosUnterseiteStyle.nummerText}>112</Text>
                 <TouchableOpacity style={sosUnterseiteStyle.telefonButton} onPress={triggerCall}>
-                    <Image source={require('../../assets/SosHomeScreen/sos_telefon.png')} style={sosUnterseiteStyle.telefon} />
+                    <Image
+                        source={require("../../assets/SosHomeScreen/sos_telefon.png")}
+                        style={sosUnterseiteStyle.telefon}
+                    />
                 </TouchableOpacity>
             </View>
             <View>
