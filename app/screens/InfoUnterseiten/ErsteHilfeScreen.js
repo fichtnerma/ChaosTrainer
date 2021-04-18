e91fc163ba8249b4abb53a6760c22063f6c0fc6c
Tabea Schaeffer <schaefferta77456@th-nuernberg.de>
2021-04-18 22:03:22 +0200

    erstehilfe text eingefügt

--- app/screens/InfoUnterseiten/ErsteHilfeScreen.js
+++ app/screens/InfoUnterseiten/ErsteHilfeScreen.js
@@ -1,9 +1,9 @@
-import React, {useState} from 'react';
-import {Text, StyleSheet, View, Image} from 'react-native';
-import {ScrollView, TouchableOpacity} from 'react-native-gesture-handler';
-import CustomModal from '../../components/CustomModal';
+import React, {useState} from "react";
+import {Text, StyleSheet, View, Image} from "react-native";
+import {ScrollView, TouchableOpacity} from "react-native-gesture-handler";
+import CustomModal from "../../components/CustomModal";
 
-const data = require('../../assets/Content/InfoSeiten/ErsteHilfe.json');
+const data = require("../../assets/Content/InfoSeiten/ErsteHilfe.json");
 const currentData = data.ersteHilfe.categories;
 const modal = React.createRef();
 
@@ -30,7 +30,10 @@ export default function ErsteHilfeScreen({navigation}) {
             </View>
             <TouchableOpacity style={kacheln.infoBox}>
                 <Text style={kacheln.infoText}>Rufe die 112 oder 110 an</Text>
-                <Image source={require('../../assets/InfoScreen/Ausrufezeichen.png')} style={kacheln.ausrufezeichen} />
+                <Image
+                    source={require("../../assets/InfoScreen/Ausrufezeichen.png")}
+                    style={kacheln.ausrufezeichen}
+                />
             </TouchableOpacity>
 
             <ScrollView>{listItems}</ScrollView>
@@ -42,39 +45,39 @@ export default function ErsteHilfeScreen({navigation}) {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'flex-start',
-        flexDirection: 'column',
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "flex-start",
+        flexDirection: "column",
         borderTopLeftRadius: 30,
         borderTopRightRadius: 30,
     },
     background: {
         flex: 1,
-        width: '100%',
-        height: '100%',
+        width: "100%",
+        height: "100%",
     },
 
     box: {
         width: 330,
         height: 60,
-        backgroundColor: '#fff',
+        backgroundColor: "#fff",
         borderRadius: 15,
         margin: 10,
         shadowOffset: {width: 0, height: 0},
-        shadowColor: 'black',
+        shadowColor: "black",
         shadowOpacity: 0.5,
-        justifyContent: 'center',
-        alignContent: 'center',
+        justifyContent: "center",
+        alignContent: "center",
         //shadowColor: '#000',
         //shadowOpacity: 0.2,
         shadowRadius: 5,
         elevation: 10,
-        alignItems: 'center',
+        alignItems: "center",
     },
     layout: {
-        height: '28%',
-        flexDirection: 'row',
+        height: "28%",
+        flexDirection: "row",
         margin: 10,
     },
 });
@@ -83,42 +86,42 @@ const kacheln = StyleSheet.create({
     infoBox: {
         width: 330,
         height: 140,
-        backgroundColor: '#fff',
+        backgroundColor: "#fff",
         borderRadius: 15,
         margin: 10,
         shadowOffset: {width: 0, height: 0},
-        shadowColor: 'black',
+        shadowColor: "black",
         shadowOpacity: 0.5,
-        justifyContent: 'center',
-        alignContent: 'center',
+        justifyContent: "center",
+        alignContent: "center",
         //shadowColor: '#000',
         //shadowOpacity: 0.2,
         shadowRadius: 5,
         elevation: 10,
-        alignItems: 'center',
+        alignItems: "center",
     },
     titel: {
         margin: 10,
         marginLeft: -163,
         marginTop: 20,
-        justifyContent: 'flex-start',
-        color: '#f79A42',
+        justifyContent: "flex-start",
+        color: "#f79A42",
         fontSize: 30,
     },
     infoText: {
-        justifyContent: 'flex-start',
-        color: '#000',
+        justifyContent: "flex-start",
+        color: "#000",
         fontSize: 16,
     },
     h2: {
-        justifyContent: 'flex-start',
-        color: '#000',
+        justifyContent: "flex-start",
+        color: "#000",
         fontSize: 24,
     },
     ausrufezeichen: {
         width: 80,
         height: 190,
-        position: 'absolute',
+        position: "absolute",
         top: 0,
         left: 0,
     },
