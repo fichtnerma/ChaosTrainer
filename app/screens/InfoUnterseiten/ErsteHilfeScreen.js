a20c74a444dc0c06531214efa11df6b9bbf37603
Tabea Schaeffer <schaefferta77456@th-nuernberg.de>
2021-04-30 16:42:42 +0200

    anpassungen

--- app/screens/InfoUnterseiten/ErsteHilfeScreen.js
+++ app/screens/InfoUnterseiten/ErsteHilfeScreen.js
@@ -1,31 +1,29 @@
-import React, {useState} from 'react';
-import {Text, StyleSheet, View, Image} from 'react-native';
-import {ScrollView, TouchableOpacity} from 'react-native-gesture-handler';
-import CustomModal from '../../components/CustomModal';
+import React, {useState} from "react";
+import {Text, StyleSheet, View, Image} from "react-native";
+import {ScrollView, TouchableOpacity} from "react-native-gesture-handler";
+import CustomModal from "../../components/CustomModal";
 
-const ersteHilfe = require('../../assets/Content/InfoSeiten/ErsteHilfe.json');
-const brand = require('../../assets/Content/InfoSeiten/Brand.json');
-// const terror = require('../../assets/Content/InfoSeiten/Terror.json');
-const ueberschwemmung = require('../../assets/Content/InfoSeiten/Uberschwemmung.json');
-const unfall = require('../../assets/Content/InfoSeiten/Unfall.json');
-// const virus = require('../../assets/Content/InfoSeiten/Virus.json');
+const ersteHilfe = require("../../assets/Content/InfoSeiten/ErsteHilfe.json");
+const brand = require("../../assets/Content/InfoSeiten/Brand.json");
+const terror = require("../../assets/Content/InfoSeiten/Terror.json");
+const ueberschwemmung = require("../../assets/Content/InfoSeiten/Uberschwemmung.json");
+const unfall = require("../../assets/Content/InfoSeiten/Unfall.json");
+const virus = require("../../assets/Content/InfoSeiten/Virus.json");
 const modal = React.createRef();
 
 function setPage(page) {
     switch (page) {
-        case 'ersteHilfe':
+        case "ersteHilfe":
             return ersteHilfe.inhalt;
-        case 'brand':
+        case "brand":
             return brand.inhalt;
-        case 'terror':
-            return ersteHilfe.inhalt;
+        case "terror":
             return terror.inhalt;
-        case 'ueberschwemmung':
+        case "ueberschwemmung":
             return ueberschwemmung.inhalt;
-        case 'unfall':
+        case "unfall":
             return unfall.inhalt;
-        case 'virus':
-            return ersteHilfe.inhalt;
+        case "virus":
             return virus.inhalt;
         default:
             return ersteHilfe.inhalt;
@@ -57,7 +55,10 @@ export default function ErsteHilfeScreen({route, navigation}) {
             </View>
             <TouchableOpacity style={kacheln.infoBox}>
                 <Text style={kacheln.infoText}>{currentData.infoText}</Text>
-                <Image source={require('../../assets/InfoScreen/Ausrufezeichen.png')} style={kacheln.ausrufezeichen} />
+                <Image
+                    source={require("../../assets/InfoScreen/Ausrufezeichen.png")}
+                    style={kacheln.ausrufezeichen}
+                />
             </TouchableOpacity>
 
             <ScrollView>{listItems}</ScrollView>
@@ -69,39 +70,39 @@ export default function ErsteHilfeScreen({route, navigation}) {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'flex-start',
-        flexDirection: 'column',
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "flex-start",
+        flexDirection: "column",
         borderTopLeftRadius: 30,
         borderTopRightRadius: 30,
     },
     background: {
         flex: 1,
-        width: '100%',
-        height: '100%',
+        width: "100%",
+        height: "100%",
     },
 
     box: {
         width: 330,
         height: 60,
-        backgroundColor: '#fff',
+        backgroundColor: "#fff",
         borderRadius: 15,
         margin: 10,
         shadowOffset: {width: 0, height: 0},
-        shadowColor: 'black',
+        shadowColor: "black",
         shadowOpacity: 0.5,
-        justifyContent: 'center',
-        alignContent: 'center',
+        justifyContent: "center",
+        alignContent: "center",
         //shadowColor: '#000',
         //shadowOpacity: 0.2,
         shadowRadius: 5,
         elevation: 10,
-        alignItems: 'center',
+        alignItems: "center",
     },
     layout: {
-        height: '28%',
-        flexDirection: 'row',
+        height: "28%",
+        flexDirection: "row",
         margin: 10,
     },
 });
@@ -109,43 +110,44 @@ const styles = StyleSheet.create({
 const kacheln = StyleSheet.create({
     infoBox: {
         width: 330,
-        height: 140,
-        backgroundColor: '#fff',
+        height: 180,
+        backgroundColor: "#fff",
         borderRadius: 15,
         margin: 10,
         shadowOffset: {width: 0, height: 0},
-        shadowColor: 'black',
+        shadowColor: "black",
         shadowOpacity: 0.5,
-        justifyContent: 'center',
-        alignContent: 'center',
+        justifyContent: "center",
+        alignContent: "center",
         //shadowColor: '#000',
         //shadowOpacity: 0.2,
         shadowRadius: 5,
         elevation: 10,
-        alignItems: 'center',
+        alignItems: "center",
     },
     titel: {
         margin: 10,
         marginLeft: -163,
         marginTop: 20,
-        justifyContent: 'flex-start',
-        color: '#f79A42',
+        justifyContent: "flex-start",
+        color: "#f79A42",
         fontSize: 30,
     },
     infoText: {
-        justifyContent: 'flex-start',
-        color: '#000',
+        justifyContent: "flex-start",
+        color: "#000",
         fontSize: 16,
+        marginLeft: 80,
     },
     h2: {
-        justifyContent: 'flex-start',
-        color: '#000',
+        justifyContent: "flex-start",
+        color: "#000",
         fontSize: 24,
     },
     ausrufezeichen: {
         width: 80,
         height: 190,
-        position: 'absolute',
+        position: "absolute",
         top: 0,
         left: 0,
     },
