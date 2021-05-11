e8808c8ccb163b11079a66df94c31c8301fad2f1
Tabea Schaeffer <schaefferta77456@th-nuernberg.de>
2021-05-11 23:40:58 +0200

    Quiz und Ecken

--- app/components/News/News.js
+++ app/components/News/News.js
@@ -1,13 +1,6 @@
 //Librarys
-import React, { useEffect, useState } from "react";
-import {
-  ActivityIndicator,
-  FlatList,
-  Image,
-  Text,
-  View,
-  StyleSheet,
-} from "react-native";
+import React, {useEffect, useState} from "react";
+import {ActivityIndicator, FlatList, Image, Text, View, StyleSheet} from "react-native";
 
 //Styles
 import mainStyle from "../Styles/mainStyleSheet.js";
@@ -15,89 +8,95 @@ import homeStyle from "../Styles/homeStyleSheet.js";
 import colors from "../../constants/colors.js";
 
 export default function News() {
-  const [isLoading, setLoading] = useState(true);
-  const [data, setData] = useState([]);
-  const renderItem = ({ item }) => (
-    <View style={[mainStyle.box, homeStyle.boxSize]}>
-      <Text style={[homeStyle.tippDesTagesTitel, styles.newsTitel]}>
-        {item.attributes.LAN_ew_GEN}
-      </Text>
-      <View style={{ flexDirection: "row" }}>
-        <Image
-          source={require("../../assets/InfoScreen/Virus.png")}
-          style={styles.virusIcon}
-        ></Image>
-        <View style={styles.newsBlock}>
-          <Text style={styles.newsText}>
-            7-Tagesinzidenz:{" "}
-            <Text style={styles.zahlenText}>
-              {item.attributes.cases7_bl_per_100k_txt}
+    const [isLoading, setLoading] = useState(true);
+    const [data, setData] = useState([]);
+    const renderItem = ({item, index}) => (
+        <View style={[mainStyle.box, homeStyle.boxSize, {marginTop: 10}]} key={index}>
+            <Text style={[homeStyle.tippDesTagesTitel, styles.newsTitel]} key={"land" + index}>
+                {item.attributes.LAN_ew_GEN}
             </Text>
-          </Text>
-          <Text style={styles.newsText}>
-            Fallzahlen:{" "}
-            <Text style={styles.zahlenText}>{item.attributes.Fallzahl}</Text>
-          </Text>
-          <Text style={styles.newsText}>
-            Todeszahlen:{" "}
-            <Text style={styles.zahlenText}>{item.attributes.Death}</Text>
-          </Text>
+            <View style={{flexDirection: "row"}} key={index}>
+                <Image
+                    source={require("../../assets/InfoScreen/Virus.png")}
+                    style={styles.virusIcon}
+                    key={"image" + index}
+                ></Image>
+                <View style={styles.newsBlock} key={index}>
+                    <Text style={styles.newsText} key={"7" + index}>
+                        7-Tagesinzidenz:{" "}
+                        <Text style={styles.zahlenText} key={"inz" + index}>
+                            {item.attributes.cases7_bl_per_100k_txt}
+                        </Text>
+                    </Text>
+                    <Text style={styles.newsText} key={"fallz" + index}>
+                        Fallzahlen:{" "}
+                        <Text style={styles.zahlenText} key={"fall" + index}>
+                            {item.attributes.Fallzahl}
+                        </Text>
+                    </Text>
+                    <Text style={styles.newsText} key={"todes" + index}>
+                        Todeszahlen:{" "}
+                        <Text style={styles.zahlenText} key={"todeszahl" + index}>
+                            {item.attributes.Death}
+                        </Text>
+                    </Text>
+                </View>
+            </View>
         </View>
-      </View>
-    </View>
-  );
-  useEffect(() => {
-    fetch(
-      "https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/Coronaf%C3%A4lle_in_den_Bundesl%C3%A4ndern/FeatureServer/0/query?where=1%3D1&outFields=OBJECTID_1,LAN_ew_AGS,LAN_ew_GEN,LAN_ew_BEZ,LAN_ew_EWZ,OBJECTID,Fallzahl,Aktualisierung,AGS_TXT,GlobalID,faelle_100000_EW,Death,cases7_bl_per_100k,cases7_bl,death7_bl,cases7_bl_per_100k_txt,AdmUnitId&returnGeometry=false&outSR=0&f=json"
-    )
-      .then((response) => response.json())
-      .then((json) => setData(json.features))
-      .catch((error) => console.error(error))
-      .finally(() => setLoading(false));
-  }, []);
+    );
+    useEffect(() => {
+        fetch(
+            "https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/Coronaf%C3%A4lle_in_den_Bundesl%C3%A4ndern/FeatureServer/0/query?where=1%3D1&outFields=OBJECTID_1,LAN_ew_AGS,LAN_ew_GEN,LAN_ew_BEZ,LAN_ew_EWZ,OBJECTID,Fallzahl,Aktualisierung,AGS_TXT,GlobalID,faelle_100000_EW,Death,cases7_bl_per_100k,cases7_bl,death7_bl,cases7_bl_per_100k_txt,AdmUnitId&returnGeometry=false&outSR=0&f=json"
+        )
+            .then((response) => response.json())
+            .then((json) => setData(json.features))
+            .catch((error) => console.error(error))
+            .finally(() => setLoading(false));
+    }, []);
 
-  function filterData(apiData) {
-    return apiData.filter((api) => api.attributes.LAN_ew_GEN == "Bayern");
-  }
-  return (
-    <View style={{ paddingTop: 10, height: 215, marginBottom: 0 }}>
-      {isLoading ? (
-        <ActivityIndicator />
-      ) : (
-        <FlatList
-          showsHorizontalScrollIndicator={false}
-          horizontal={true}
-          data={data}
-          keyExtractor={({ id }) => id}
-          renderItem={renderItem}
-        />
-      )}
-    </View>
-  );
+    // function filterData(apiData) {
+    //     return apiData.filter((api) => api.attributes.LAN_ew_GEN == "Bayern");
+    // }
+
+    return (
+        <View style={{paddingTop: 10, height: 215, marginBottom: 0}}>
+            {isLoading ? (
+                <ActivityIndicator />
+            ) : (
+                <FlatList
+                    showsHorizontalScrollIndicator={false}
+                    horizontal={true}
+                    data={data}
+                    keyExtractor={({id}) => id}
+                    renderItem={renderItem}
+                />
+            )}
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  virusIcon: {
-    width: 120,
-    height: 120,
-  },
+    virusIcon: {
+        width: 120,
+        height: 120,
+    },
 
-  newsBlock: {
-    marginTop: 20,
-  },
+    newsBlock: {
+        marginTop: 20,
+    },
 
-  newsText: {
-    fontSize: 16,
-  },
+    newsText: {
+        fontSize: 16,
+    },
 
-  newsTitel: {
-    top: 5,
-    fontSize: 25,
-  },
+    newsTitel: {
+        top: 5,
+        fontSize: 25,
+    },
 
-  zahlenText: {
-    fontSize: 22,
-    fontWeight: "bold",
-    color: colors.blue,
-  },
+    zahlenText: {
+        fontSize: 22,
+        fontWeight: "bold",
+        color: colors.blue,
+    },
 });
--- app/components/Styles/homeStyleSheet.js
+++ app/components/Styles/homeStyleSheet.js
@@ -1,57 +1,57 @@
 //Librarys
-import { StyleSheet, Dimensions } from "react-native";
+import {StyleSheet, Dimensions} from "react-native";
 import Colors from "../../constants/colors.js";
 
 const windowWidth = Dimensions.get("window").width;
 const windowHeight = Dimensions.get("window").height;
 
 export default StyleSheet.create({
-  boxSize: {
-    width: windowWidth * 0.9,
-    height: windowHeight * 0.245,
-    marginTop: 20,
-  },
+    boxSize: {
+        width: windowWidth * 0.9,
+        height: windowHeight * 0.245,
+        marginTop: 20,
+    },
 
-  tippDesTagesBox: {
-    width: windowWidth * 0.9,
-    height: 140,
-    backgroundColor: Colors.white,
-    borderRadius: 15,
-    margin: 10,
-    shadowOffset: { width: 0, height: 0 },
-    shadowColor: "black",
-    shadowOpacity: 0.5,
-    justifyContent: "center",
-    alignContent: "center",
-    shadowRadius: 5,
-    elevation: 10,
-  },
+    tippDesTagesBox: {
+        width: windowWidth * 0.9,
+        height: 140,
+        backgroundColor: Colors.white,
+        borderRadius: 15,
+        margin: 10,
+        shadowOffset: {width: 0, height: 0},
+        shadowColor: "black",
+        shadowOpacity: 0.5,
+        justifyContent: "center",
+        alignContent: "center",
+        shadowRadius: 5,
+        elevation: 10,
+    },
 
-  //Text
-  tippDesTagesTitel: {
-    color: Colors.orange,
-    fontSize: 25,
-    marginLeft: 15,
-    marginBottom: 5,
-    top: 8,
-    position: "absolute",
-    //fontWeight: "bold",
-  },
+    //Text
+    tippDesTagesTitel: {
+        color: Colors.orange,
+        fontSize: 25,
+        marginLeft: 15,
+        marginBottom: 5,
+        top: 8,
+        position: "absolute",
+        //fontWeight: "bold",
+    },
 
-  tippDesTagesText: {
-    color: Colors.black,
-    fontSize: 16,
-    marginLeft: 15,
-    marginRight: 100,
-    marginTop: 25,
-  },
+    tippDesTagesText: {
+        color: Colors.black,
+        fontSize: 16,
+        marginLeft: 15,
+        marginRight: 100,
+        marginTop: 25,
+    },
 
-  //Images
-  tippDesTagesImage: {
-    width: 100,
-    height: 90,
-    position: "absolute",
-    top: 85,
-    left: 220,
-  },
+    //Images
+    tippDesTagesImage: {
+        width: 100,
+        height: 90,
+        position: "absolute",
+        top: 85,
+        left: 220,
+    },
 });
