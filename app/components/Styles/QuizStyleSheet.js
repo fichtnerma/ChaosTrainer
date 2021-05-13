8b6c64a5934e5bb0ac09eec7a4940dbfe95f57b1
Markus Fichtner <fichtnerma76137@th-nuernberg.de>
2021-05-13 16:12:07 +0200

    Einstellungen Modal

--- app/components/Styles/QuizStyleSheet.js
+++ app/components/Styles/QuizStyleSheet.js
@@ -1,11 +1,11 @@
 //Librarys
-import {StyleSheet, Dimensions} from "react-native";
+import {StyleSheet, Dimensions} from 'react-native';
 
 //Constants
-import Colors from "../../constants/colors.js";
+import Colors from '../../constants/colors.js';
 
-const windowWidth = Dimensions.get("window").width;
-const windowHeight = Dimensions.get("window").height;
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 
 export default StyleSheet.create({
     boxSize: {
@@ -13,8 +13,8 @@ export default StyleSheet.create({
         height: windowHeight * 0.4,
     },
     layout: {
-        height: "80%",
-        flexDirection: "column",
+        height: '80%',
+        flexDirection: 'column',
         margin: 10,
     },
 
@@ -22,23 +22,53 @@ export default StyleSheet.create({
     ShadowText: {
         color: Colors.white,
         fontSize: 70,
-        fontWeight: "bold",
+        fontWeight: 'bold',
         marginLeft: 40,
-        alignSelf: "flex-start",
-        position: "absolute",
+        alignSelf: 'flex-start',
+        position: 'absolute',
         top: -4,
         zIndex: 1,
         textShadowOffset: {width: 0, height: 0},
-        textShadowColor: "#CCC",
+        textShadowColor: '#CCC',
         textShadowRadius: 15,
-        shadowOpacity: 0.5,
-        elevation: 1,
+        shadowOpacity: 0.2,
+    },
+    modal: {
+        backgroundColor: '#fff',
+        borderTopRightRadius: 30,
+        borderTopLeftRadius: 30,
+        width: windowWidth,
+        height: windowHeight * 0.76,
+        alignItems: 'center',
+        shadowColor: Colors.black,
+        zIndex: 99,
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.2,
+        shadowRadius: 4,
+        elevation: 5,
+    },
+    save: {
+        width: windowWidth * 0.7,
+        height: 50,
+        marginTop: windowHeight * 0.08,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: Colors.blue,
+        borderRadius: 20,
+    },
+
+    saveText: {
+        color: '#fff',
+        fontSize: 22,
     },
 
     hindText: {
         marginTop: 0,
         fontSize: 20,
-        textAlignVertical: "center",
+        textAlignVertical: 'center',
         marginLeft: 20,
         color: Colors.lightOrange,
     },
@@ -46,7 +76,7 @@ export default StyleSheet.create({
     hiScoreText: {
         color: Colors.white,
         fontSize: 15,
-        textAlignVertical: "center",
+        textAlignVertical: 'center',
         marginLeft: 80,
         marginTop: 5,
     },
@@ -54,15 +84,15 @@ export default StyleSheet.create({
     hiScore: {
         color: Colors.white,
         fontSize: 40,
-        textAlignVertical: "center",
+        textAlignVertical: 'center',
         marginLeft: 80,
-        fontWeight: "bold",
+        fontWeight: 'bold',
     },
     startText: {
         color: Colors.white,
         fontSize: 25,
-        textAlignVertical: "center",
-        textAlign: "center",
+        textAlignVertical: 'center',
+        textAlign: 'center',
     },
 
     //Buttons
@@ -71,7 +101,7 @@ export default StyleSheet.create({
         width: 230,
         height: 80,
         marginTop: 70,
-        overflow: "visible",
+        overflow: 'visible',
     },
     settingButton: {
         backgroundColor: Colors.orange,
@@ -84,26 +114,26 @@ export default StyleSheet.create({
     settingImage: {
         width: 50,
         height: 50,
-        alignSelf: "center",
+        alignSelf: 'center',
     },
     feuerImage: {
         width: 50,
         height: 70,
-        alignSelf: "center",
+        alignSelf: 'center',
         marginBottom: 70,
         marginLeft: 10,
     },
     QuizPersonImage: {
         width: 110,
         height: 150,
-        position: "absolute",
+        position: 'absolute',
         top: -69,
         left: -15,
         zIndex: 40,
     },
 
     textInput: {
-        justifyContent: "center",
+        justifyContent: 'center',
         color: Colors.orange,
         fontSize: 22,
         marginTop: 10,
@@ -115,18 +145,33 @@ export default StyleSheet.create({
 
     centeredView: {
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        justifyContent: 'flex-start',
+        alignItems: 'center',
     },
     modalView: {
-        justifyContent: "flex-start",
-        width: windowWidth * 0.92,
-        height: windowHeight * 0.65,
+        borderTopRightRadius: 30,
+        borderTopLeftRadius: 30,
+        justifyContent: 'flex-start',
+        width: windowWidth,
+        height: windowHeight * 0.76,
+        alignItems: 'center',
+        padding: 10,
     },
     kategorienContainer: {
         backgroundColor: Colors.lightOrange,
-        height: windowHeight * 0.36,
+        height: windowHeight * 0.4,
+        width: windowWidth,
+        borderTopRightRadius: 30,
+        borderTopLeftRadius: 30,
     },
+    nrQuestions: {
+        flexDirection: 'row',
+        borderTopRightRadius: 30,
+        borderTopLeftRadius: 30,
+        backgroundColor: '#fff',
+        marginTop: -30,
+    },
+
     kategorieText: {
         color: Colors.white,
         fontSize: 25,
